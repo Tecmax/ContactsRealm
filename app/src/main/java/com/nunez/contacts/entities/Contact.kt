10d324c8ca8c24c4b0d58032e99acc0db913d3a6
Paul Nunez <paulnunezm@gmail.com>
2017-05-21 00:35:23 -0400

    added: contact object.

---
+++ app/src/main/java/com/nunez/contacts/entities/Contact.kt
@@ -0,0 +1,8 @@
+package com.nunez.contacts.entities
+
+class Contact(
+        var firstName: String,
+        var lastName: String,
+        var phoneNumber: String,
+        var birthday: String,
+        var zipCode: String)
