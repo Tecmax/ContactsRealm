4a187bc5c08ed868a8d9242700b5c810cb8e5fba
Paul Nunez <paulnunezm@gmail.com>
2017-05-21 01:05:23 -0400

    added: contacts repository

---
+++ app/src/main/java/com/nunez/contacts/repository/ConcactsRepository.kt
@@ -0,0 +1,25 @@
+package com.nunez.contacts.repository
+
+import com.nunez.contacts.entities.Contact
+
+class ConcactsRepository: ContactsRepositoryTemplate {
+    override fun create(contact: Contact) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun read(contactId: Int): Contact {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun read(): List<Contact> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun update(contact: Contact) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(contact: Contact) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
