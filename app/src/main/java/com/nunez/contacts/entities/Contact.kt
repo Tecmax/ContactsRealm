e2c188c5c5df03d11254267430d967193d95efe9
Paul Nunez <paulnunezm@gmail.com>
2017-05-22 11:26:53 -0400

    edit: change data keyword to open.
    Realm classes must be able to be extended. The Realm library handles the creation
    of getter and setters.

--- app/src/main/java/com/nunez/contacts/entities/Contact.kt
+++ app/src/main/java/com/nunez/contacts/entities/Contact.kt
@@ -3,11 +3,11 @@ package com.nunez.contacts.entities
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-data class Contact (
-    @PrimaryKey var id: Int,
-    var firstName: String,
-    var lastName: String,
-    var phoneNumber: String,
-    var birthday: String,
-    var zipCode: String
-): RealmObject()
+open class Contact(
+        @PrimaryKey var id: String = "",
+        var firstName: String = "",
+        var lastName: String = "",
+        var phoneNumber: String = "",
+        var birthday: String = "",
+        var zipCode: String = ""
+) : RealmObject()
