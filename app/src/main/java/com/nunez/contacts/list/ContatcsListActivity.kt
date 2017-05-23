b54c01f87e1557f7aa8b8fcba71626489f13fad7
Paul Nunez <paulnunezm@gmail.com>
2017-05-22 21:28:58 -0400

    edit: move contacts list activity to new package.
     To organize to project by feature.

--- app/src/main/java/com/nunez/contacts/list/ContatcsListActivity.kt
+++ app/src/main/java/com/nunez/contacts/list/ContatcsListActivity.kt
@@ -1,4 +1,4 @@
-package com.nunez.contacts
+package com.nunez.contacts.list
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import com.nunez.contacts.R
 
 class ContatcsListActivity : AppCompatActivity() {
 
