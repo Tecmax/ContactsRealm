cc9300cd0733f5f84176e8f3eaa2b08f53531165
Paul Nunez <paulnunezm@gmail.com>
2017-05-22 22:35:14 -0400

    edit: renamed contacts list activity and added recyclerView to the layout.

--- app/src/main/java/com/nunez/contacts/list/ContactsListActivity.kt
+++ app/src/main/java/com/nunez/contacts/list/ContactsListActivity.kt
@@ -1,26 +1,24 @@
 package com.nunez.contacts.list
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import com.nunez.contacts.R
+import com.nunez.contacts.common.ContactsAdapter
+import kotlinx.android.synthetic.main.contacts_list_activity.*
 
-class ContatcsListActivity : AppCompatActivity() {
+class ContactsListActivity : AppCompatActivity() {
+    lateinit var adapter: ContactsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contacts_list_activity)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        addContact.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
         }
     }
 
