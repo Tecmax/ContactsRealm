a90967d6276b5df5efea460828fa21c053d10def
Paul Nunez <paulnunezm@gmail.com>
2017-05-22 22:28:39 -0400

    added: contacts adapter.

---
+++ app/src/main/java/com/nunez/contacts/common/ContactsAdapter.kt
@@ -0,0 +1,33 @@
+package com.nunez.contacts.common
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.nunez.contacts.R
+import com.nunez.contacts.entities.Contact
+import kotlinx.android.synthetic.main.contact_item.view.*
+
+class ContactsAdapter(
+        var contacts: List<Contact>,
+        var onClickListener: (String) -> (Unit)
+) : RecyclerView.Adapter<ContactsAdapter.ContactViewHolder>() {
+
+    override fun getItemCount() = contacts.size
+
+    override fun onBindViewHolder(holder: ContactViewHolder?, position: Int) {
+        holder?.let { it.bind(contacts[position]) }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        return ContactViewHolder(parent.inflate(R.layout.contact_item), onClickListener)
+    }
+
+    class ContactViewHolder(val view: View, val contactClickListener: (String) -> Unit) : RecyclerView.ViewHolder(view) {
+        fun bind(contact: Contact) {
+            with(contact) {
+                view.contactName.text = "$firstName  $lastName"
+                view.setOnClickListener { contactClickListener(id) }
+            }
+        }
+    }
+}
