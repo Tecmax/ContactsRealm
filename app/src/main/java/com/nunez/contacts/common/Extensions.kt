cdbc17212591c664ac292861c8f1de9d6feb5bc7
Paul Nunez <paulnunezm@gmail.com>
2017-05-23 19:26:39 -0400

    added: show snackbar extension.

--- app/src/main/java/com/nunez/contacts/common/Extensions.kt
+++ app/src/main/java/com/nunez/contacts/common/Extensions.kt
@@ -1,5 +1,6 @@
 package com.nunez.contacts.common
 
+import android.support.design.widget.Snackbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,4 +10,8 @@ import android.view.ViewGroup
  */
 fun ViewGroup.inflate(resource: Int): View {
     return LayoutInflater.from(this.context).inflate(resource, this, false)
+}
+
+fun showSnackbar(view: View, message: String, duration: Int = Snackbar.LENGTH_SHORT) {
+    Snackbar.make(view, message, duration).show()
 }
