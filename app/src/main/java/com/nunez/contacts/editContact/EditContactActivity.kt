c9dfa703a5cc32c338a7b635caa78feac3fbb0c3
Paul Nunez <paulnunezm@gmail.com>
2017-05-24 15:48:30 -0400

    added: edit contacts activity.

---
+++ app/src/main/java/com/nunez/contacts/editContact/EditContactActivity.kt
@@ -0,0 +1,13 @@
+package com.nunez.contacts.editContact
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nunez.contacts.R
+
+class EditContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_contact_layout)
+    }
+}
