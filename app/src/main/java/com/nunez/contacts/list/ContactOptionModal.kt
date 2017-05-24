b44d229349e46a5d0e248b43e89936115580328c
Paul Nunez <paulnunezm@gmail.com>
2017-05-24 17:58:16 -0400

    edit: change option modal to extend BottomSheetDialogFragment  and now we inflate the layout with the inflater instead of the container.

--- app/src/main/java/com/nunez/contacts/list/ContactOptionModal.kt
+++ app/src/main/java/com/nunez/contacts/list/ContactOptionModal.kt
@@ -1,22 +1,21 @@
 package com.nunez.contacts.list
 
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
+import android.support.design.widget.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nunez.contacts.R
-import com.nunez.contacts.common.inflate
 import kotlinx.android.synthetic.main.contact_option_modal.*
 
 class ContactOptionModal(
         val contactId: String,
         val editClicked: (String) -> Unit,
         val deleteClicked: (String) -> Unit
-) : DialogFragment() {
+) : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return container?.inflate(R.layout.contact_option_modal)
+        return inflater?.inflate(R.layout.contact_option_modal, container)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
