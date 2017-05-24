ddb2b09b81ffd1bb1c67013c4c1a178d1f043f2f
Paul Nunez <paulnunezm@gmail.com>
2017-05-24 17:56:14 -0400

    edit: added options modal and tag val.

--- app/src/main/java/com/nunez/contacts/list/ContactsListActivity.kt
+++ app/src/main/java/com/nunez/contacts/list/ContactsListActivity.kt
@@ -2,14 +2,12 @@ package com.nunez.contacts.list
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import com.nunez.contacts.R
 import com.nunez.contacts.common.ContactsAdapter
-import com.nunez.contacts.common.showSnackbar
 import com.nunez.contacts.editContact.EditContactActivity
 import com.nunez.contacts.entities.Contact
 import com.nunez.contacts.repository.ContactsRepository
@@ -20,6 +18,7 @@ import kotlin.reflect.KClass
 class ContactsListActivity : AppCompatActivity(), ContactListContract.View {
 
     companion object {
+        const val TAG = "ContactsListActivity"
         const val EXTRA_CONTACT_ID = "contact_id"
     }
 
@@ -77,15 +76,16 @@ class ContactsListActivity : AppCompatActivity(), ContactListContract.View {
     }
 
     override fun showContactDetails(id: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun showErrorMessage() {
-        showSnackbar(contactsRecycler, getString(R.string.message_error), Snackbar.LENGTH_SHORT)
     }
 
-    override fun showDeleteBottomSheet(id: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun showOptionsModalBottomSheet(id: String) {
+        val modal = ContactOptionModal(id,
+                {presenter.contactToEditClicked(it)},
+                {presenter.contactToDeleteClicked(it)}
+        ).show(supportFragmentManager, "modal")
     }
 
     override fun goToActivity(contactId: String, activityClass: KClass<EditContactActivity>) {
